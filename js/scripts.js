4b0f32b87f04a2ada30279e0dc48abe755959eb8
Stewart ilondanga <stilondanga@gmail.com>
2018-12-31 17:56:23 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,18 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+function form_in(){
+   document.getElementById("Sign_In").classList.add("show_form")
+   document.getElementById("Sign_Up").classList.remove("show_form")
+}
+
+function form_up(){
+   document.getElementById("Sign_Up").classList.add("show_form")
+   document.getElementById("Sign_In").classList.remove("show_form")
+}
+
+function close_form(){
+   document.getElementById("Sign_Up").classList.remove("show_form")
+   document.getElementById("Sign_In").classList.remove("show_form")
+}
